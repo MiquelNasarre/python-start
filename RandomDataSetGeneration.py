b86918389aac1577563aec47b4ad4f809fdeea78
Miquel <miguel.nasarre.budino@gmail.com>
2023-02-22 11:24:05 +0100

    First commit working gradient descent test

---
+++ RandomDataSetGeneration.py
@@ -0,0 +1,29 @@
+#import numpy as np
+import random as rd
+
+def GenerateVector(Dimension):
+    v = list()
+    for i in range(Dimension + 1):
+        v.insert(v.__len__(), rd.randint(0,100) / 100)
+    return v
+
+def RandomDataSet(deviation, InputDimension, Samples):
+    theta = GenerateVector(InputDimension)
+    filename = ""
+    for value in theta:
+        filename += "{:.2f}_".format(value)
+    File = open("DataFiles/{}.dat".format(filename), "w")
+    
+    for ignore in range(Samples):
+        x = GenerateVector(InputDimension)
+        x[0] = 1
+        y = 0
+        for i , j in zip(x , theta):
+            y += i * j
+        for i in x[1:]:
+            File.write("{:.2f}\t".format(i))
+        File.write("{:.4f}\n".format(y + deviation * (rd.randint(0,100) / 100 - 0.5)))
+    return theta, "DataFiles/{}.dat".format(filename)
+
+if __name__ == '__main__':
+    RandomDataSet(0,5,100)
---
+++ test.py
@@ -0,0 +1,15 @@
+import FirstProperML as ml
+import RandomDataSetGeneration as rg
+
+deviation = 0.01
+dimension = 10
+sampleSize = 500
+alpha = 0.01
+accuracy = 0.005
+
+theta , filename = rg.RandomDataSet(deviation, dimension, sampleSize)
+print("Generated theta's with deviation {}:".format(deviation))
+for i, value in enumerate(theta):
+    print("\ttheta{:} = {:.2f}".format(i, value))
+
+ml.GradientDescent(ml.ReadFileData(open(filename, "r"), dimension), alpha, accuracy)
